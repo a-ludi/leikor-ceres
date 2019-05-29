f7f343b8692988ab9c1b9db959c6e8f86a7f6c3d
Arne Ludwig <arne.ludwig@posteo.de>
2019-05-29 16:00:06 +0200

    SingleItem

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -23,7 +23,7 @@ class ThemeServiceProvider extends ServiceProvider
      */
     public function boot(Twig $twig, Dispatcher $eventDispatcher)
     {
-        $eventDispatcher->listen('IO.tpl.basket', function(TemplateContainer $container, $templateData)
+        $eventDispatcher->listen('IO.tpl.item', function(TemplateContainer $container, $templateData)
         {
             $container->setTemplate('LEIKOR::Item.SingleItemWrapper');
             return false;
