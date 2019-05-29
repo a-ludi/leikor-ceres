f0d67286c1e5cb62fb3bcf0afddb3bbdf1ccd325
Arne Ludwig <arne.ludwig@posteo.de>
2019-05-29 09:40:40 +0200

    Initial commit
    - initiated plugin structure according to [the theme tutorial][tut]
    
    [tut]: https://developers.plentymarkets.com/dev-doc/theme-plugins

---
+++ src/Containers/ThemeContainer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LEIKOR\Containers;
+
+use Plenty\Plugin\Templates\Twig;
+
+class ThemeContainer
+{
+    public function call(Twig $twig):string
+    {
+        return $twig->render('LEIKOR::content.Theme');
+    }
+}
---
+++ src/Providers/ThemeServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LEIKOR\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+
+    }
+}
